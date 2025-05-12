1caa0cb7c5ff991b1ad0c40e45ffc65b0d986e06
Sogiefa-12 <131720010+Sogiefa-12@users.noreply.github.com>
2025-05-12 16:39:28 +0100

    changes made to SimpleQueue

--- week02/learn/SimpleQueue.cs
+++ week02/learn/SimpleQueue.cs
@@ -14,6 +14,8 @@
 
         Console.WriteLine("------------");
 
+
+
         // Test 2
         // Scenario: Enqueue multiple values and then Dequeue all of them
         // Expected Result: It should display 200, then 300, then 400 in that order
@@ -44,7 +46,7 @@
         catch (IndexOutOfRangeException) {
             Console.WriteLine("I got the exception as expected.");
         }
-        // Defect(s) Found: 
+        // Defect(s) Found: None :)
     }
 
     private readonly List<int> _queue = new();
@@ -54,7 +56,7 @@
     /// </summary>
     /// <param name="value">Integer value to add to the queue</param>
     private void Enqueue(int value) {
-        _queue.Insert(0, value);
+        _queue.Add(value);
     }
 
     /// <summary>
@@ -66,8 +68,8 @@
         if (_queue.Count <= 0)
             throw new IndexOutOfRangeException();
 
-        var value = _queue[1];
-        _queue.RemoveAt(1);
+        var value = _queue[0];
+        _queue.RemoveAt(0);
         return value;
     }
 }
